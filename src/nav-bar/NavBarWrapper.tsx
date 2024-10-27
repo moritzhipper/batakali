6e79f44332a72d7689b53bf1688938927992a37e
Moritz Hipper <moritz.hipper@outlook.de>
2024-10-27 12:58:27 +0100

    fixed some style

--- src/nav-bar/NavBarWrapper.tsx
+++ src/nav-bar/NavBarWrapper.tsx
@@ -2,7 +2,6 @@ import { useLocation } from "react-router-dom"
 import { useMediaQuery } from "../mediaQueryHook"
 import { NavBarDesktop } from "./NavBarDesktop"
 import { NavBarMobile } from "./NavBarMobile"
-import "./NavBarWrapper.css"
 
 export const NavBarWrapper = () => {
   const isMobile = useMediaQuery("(max-width: 700px)")
