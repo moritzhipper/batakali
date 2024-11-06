c6a9b8f2233cd7d6c56c84a14706bfe5a029b222
Moritz Hipper <moritz.hipper@outlook.de>
2024-11-06 21:52:59 +0100

    made filter look nicer

--- src/components/pages/projects/ProjectsPage.tsx
+++ src/components/pages/projects/ProjectsPage.tsx
@@ -1,4 +1,6 @@
+import { a, useSpring, useTransition } from "@react-spring/web"
 import { useState } from "react"
+import { springConfig } from "../../../angry-ducko-config"
 import { useMediaStore } from "../../../porject-media-store"
 import { Icon } from "../../Icon"
 import { PageWrapper } from "../PageWrapper"
@@ -7,11 +9,31 @@ import "./ProjectsPage.css"
 
 export const ProjectsPage = () => {
   const { projectList, selectProject, selectTag } = useMediaStore()
-  const [showFilter, setShowFilter] = useState(false)
+  const [showFilter, setShowFilter] = useState(true)
 
   const toggleFilter = () => setShowFilter((show) => !show)
-
   const tagList = [...new Set(projectList.map((project) => project.tag))].sort()
+
+  const filterStyle = {
+    opacity: 0,
+    translateY: 40
+  }
+  const transitionFilter = useTransition(showFilter, {
+    from: filterStyle,
+    enter: {
+      opacity: 1,
+      translateY: 0
+    },
+    leave: filterStyle,
+    ...springConfig
+  })
+
+  const reelStyle = useSpring({
+    opacity: showFilter ? 0.5 : 1,
+    translateY: showFilter ? 0 : 0,
+    pointerEvents: showFilter ? "none" : "all",
+    ...springConfig
+  })
 
   return (
     <PageWrapper type="half">
@@ -23,16 +45,24 @@ export const ProjectsPage = () => {
           </button>
         </h1>
         <div className="project-selector">
-          {showFilter && (
-            <div className="tag-selector-wrapper">
-              {tagList.map((tag) => (
-                <button onClick={() => selectTag(tag)} key={tag}>
-                  {tag}
-                </button>
-              ))}
-            </div>
-          )}
-          <ProjectReel projects={projectList} play={selectProject} />
+          {transitionFilter((style, show) => (
+            <>
+              {show && (
+                <a.div className="tag-selector-wrapper" style={{ ...style }}>
+                  {tagList.map((tag) => (
+                    <button onClick={() => selectTag(tag)} key={tag}>
+                      {tag}
+                    </button>
+                  ))}
+                </a.div>
+              )}
+              {!show && (
+                <a.div className="reel-wrapper" style={{ ...style }}>
+                  <ProjectReel projects={projectList} play={selectProject} />
+                </a.div>
+              )}
+            </>
+          ))}
         </div>
       </div>
     </PageWrapper>
