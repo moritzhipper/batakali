5a15d027372c0e7e68fb11a2f16dd56492cf06d4
Moritz Hipper <moritz.hipper@outlook.de>
2024-11-11 20:57:28 +0100

    refactored stuff

--- src/components/pages/projects-page/ProjectsPageWrapper.tsx
+++ src/components/pages/projects-page/ProjectsPageWrapper.tsx
@@ -5,7 +5,7 @@ import { useMediaStore } from "../../../state/audioState"
 import { useSzeneState } from "../../../state/szeneState"
 import { MediaControls } from "../../media-controls/MediaControls"
 import { ProjectReel } from "./ProjectReel"
-import "./ProjectsPage.css"
+import "./ProjectsPageWrapper.css"
 import { TagSelector } from "./TagSelector"
 
 export const ProjectsPage = () => {
@@ -38,19 +38,17 @@ export const ProjectsPage = () => {
     <a.div className="page-wrapper projects" style={{ translateY }}>
       {transitionPlayer((style, show) =>
         show ? (
-          <>
-            <a.div className="controls-wrapper" style={{ ...style }}>
-              <div className="now-playing">
-                <h1>{selectedProject.name}</h1>
-                <span className="text-shadow">{selectedProject.tag}</span>
-              </div>
-              <MediaControls />
-              <button
-                className="hide ri-arrow-up-wide-line"
-                onClick={togglePlayer}
-              />
-            </a.div>
-          </>
+          <a.div className="controls-wrapper" style={{ ...style }}>
+            <div className="now-playing">
+              <h1>{selectedProject.name}</h1>
+              <span className="text-shadow">{selectedProject.tag}</span>
+            </div>
+            <MediaControls />
+            <button
+              className="hide ri-arrow-up-wide-line"
+              onClick={togglePlayer}
+            />
+          </a.div>
         ) : (
           <a.div className="content" style={{ ...style }}>
             <SelectionElements onHide={togglePlayer} />
