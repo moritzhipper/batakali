4483f96dad05f88ab2cf5dfa573a86f1a2e2ad7d
Moritz Hipper <moritz.hipper@outlook.de>
2024-11-16 12:17:37 +0100

    added imprint and privacy

--- src/components/pages/ImprintPage.tsx
+++ src/components/pages/ImprintPage.tsx
@@ -1,7 +1,19 @@
+import "./ImprintPage.css"
+
 export const ImprintPage = () => {
   return (
-    <div className="page-wrapper">
+    <div className="page-wrapper imprint">
       <h1>Impressum</h1>
+      <div>
+        <h2>Moritz Hipper</h2>
+        <p>Augustenstraße 118</p>
+        <p>70197, Stuttgart</p>
+        <p>Germany</p>
+      </div>
+      <div className="contact">
+        <h2>Kontakt</h2>
+        <p>E-Mail: moritz.hipper@protonmail.com</p>
+      </div>
     </div>
   )
 }
