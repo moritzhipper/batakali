bf5689035bb199845093da8adb49f2a137d17843
Moritz Hipper <moritz.hipper@outlook.de>
2024-11-22 15:18:58 +0100

    more refactoring

--- src/components/three/Ducko.tsx
+++ src/components/three/Ducko.tsx
@@ -32,13 +32,19 @@ export const Ducko = memo(({ duckoConfig }: Props) => {
     []
   )
 
+  const shardGeneratorConfig: ShardGeneratorConfig[] = [
+    { amount: 10, height: 1.5, innerRadius: 5, outerRadius: 6 },
+    { amount: 30, height: 0.9, innerRadius: 5, outerRadius: 7 },
+    { amount: 30, height: 0.7, innerRadius: 6, outerRadius: 9 },
+    { amount: 80, height: 0.5, innerRadius: 9, outerRadius: 20 }
+  ]
+
   const shardList = useMemo(
-    () => [
-      ...generateRandomSpriteElements(10, 1.5, textures, 5, 6),
-      ...generateRandomSpriteElements(30, 0.9, textures, 5, 7),
-      ...generateRandomSpriteElements(30, 0.7, textures, 6, 9),
-      ...generateRandomSpriteElements(80, 0.5, textures, 9, 20)
-    ],
+    () =>
+      generateRandomShards({
+        configList: shardGeneratorConfig,
+        textures
+      }),
     []
   )
 
@@ -105,31 +111,43 @@ export const Ducko = memo(({ duckoConfig }: Props) => {
   )
 })
 
-const generateRandomSpriteElements = (
-  amount: number,
-  height: number,
-  textures: Texture[],
-  innerRadius: number,
+type ShardGeneratorConfig = {
+  height: number
+  amount: number
+  innerRadius: number
   outerRadius: number
-): JSX.Element[] => {
+}
+
+type RandomShardGeneratorFunctionInput = {
+  configList: ShardGeneratorConfig[]
+  textures: Texture[]
+}
+
+const generateRandomShards = ({
+  configList,
+  textures
+}: RandomShardGeneratorFunctionInput): JSX.Element[] => {
   const sprites = []
 
-  for (let i = 0; i < amount; i++) {
-    const randomPos = getRandomPositionInSphereWithXBias(
-      innerRadius,
-      outerRadius,
-      0.65
-    )
-    const randomTexture = textures[randomInt(0, textures.length)]
+  for (const { amount, height, innerRadius, outerRadius } of configList) {
+    for (let i = 0; i < amount; i++) {
+      const randomPos = getRandomPositionInSphereWithXBias(
+        innerRadius,
+        outerRadius,
+        0.65
+      )
+      const randomTexture = textures[randomInt(0, textures.length)]
 
-    sprites.push(
-      <ImageElement
-        position={randomPos}
-        height={height}
-        texture={randomTexture}
-        asSprite
-      />
-    )
+      sprites.push(
+        <ImageElement
+          position={randomPos}
+          height={height}
+          texture={randomTexture}
+          asSprite
+          key={sprites.length}
+        />
+      )
+    }
   }
 
   return sprites
