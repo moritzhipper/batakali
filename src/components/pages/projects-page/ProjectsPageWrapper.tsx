58061b9804418eda46a395933c78b3215e7e103b
Moritz Hipper <moritz.hipper@outlook.de>
2024-11-30 12:46:12 +0100

    wp

--- src/components/pages/projects-page/ProjectsPageWrapper.tsx
+++ src/components/pages/projects-page/ProjectsPageWrapper.tsx
@@ -1,18 +1,15 @@
 import { a, useSpring, useTransition } from "@react-spring/web"
 import { useEffect, useState } from "react"
 import { springConfig } from "../../../duckoSzeneConfig"
-import { useAudioStore } from "../../../state/audioState"
 import { useSzeneState } from "../../../state/szeneState"
 import { MediaControls } from "./MediaControls"
-import { ProjectReel } from "./ProjectReel"
 import "./ProjectsPageWrapper.css"
-import { TagSelector } from "./TagSelector"
+import { ProjectsViewMobile } from "./ProjectsViewMobile"
 
 export const ProjectsPage = () => {
   const [playerVisible, setPlayerVisible] = useState(false)
   const togglePlayer = () => setPlayerVisible((show) => !show)
   const { setActiveSzene } = useSzeneState()
-  const { selectedProject } = useAudioStore()
 
   useEffect(() => {
     if (playerVisible) {
@@ -44,67 +41,11 @@ export const ProjectsPage = () => {
             </a.div>
           ) : (
             <a.div className="content" style={{ ...style }}>
-              <SelectionElements onHide={togglePlayer} />
+              <ProjectsViewMobile onHide={togglePlayer} />
             </a.div>
           )
         )}
       </a.div>
     </div>
-  )
-}
-
-type SelectionElementsProps = {
-  onHide: () => void
-}
-
-export const SelectionElements = ({ onHide }: SelectionElementsProps) => {
-  const [showFilter, setShowFilter] = useState(false)
-  const { selectedTag } = useAudioStore()
-  const toggleFilter = () => setShowFilter((show) => !show)
-
-  useEffect(() => {
-    setShowFilter(false)
-  }, [selectedTag])
-
-  const hiddenElementStyle = {
-    opacity: 0,
-    translateY: 40
-  }
-
-  const transitionFilter = useTransition(showFilter, {
-    initial: {
-      opacity: 1,
-      translateY: 0
-    },
-    from: hiddenElementStyle,
-    enter: {
-      opacity: 1,
-      translateY: 0
-    },
-    leave: hiddenElementStyle,
-    ...springConfig
-  })
-
-  return (
-    <>
-      <h1>
-        <span>Projects</span>
-        <button className="hide" onClick={onHide}>
-          <span className="ri-play-large-fill" />
-          <span className="ri-pause-large-line" />
-        </button>
-        <button
-          className={"filter ri-filter-2-fill " + (showFilter ? "active" : "")}
-          onClick={toggleFilter}
-        />
-      </h1>
-      <div className="selector-wrapper">
-        {transitionFilter((style, show) => (
-          <a.div className="project-selector" style={{ ...style }}>
-            {show ? <TagSelector /> : <ProjectReel />}
-          </a.div>
-        ))}
-      </div>
-    </>
   )
 }
